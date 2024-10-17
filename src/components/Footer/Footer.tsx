09ab48d12ec8209ea3549649990ce3df148964bc
protoss70 <tlegendgaming@gmail.com>
2024-10-17 15:12:28 +0200

    Responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,17 +13,17 @@ const Footer: React.FC = () => {
   return (
     <footer className='relative flex flex-col justify-between w-full pb-16 text-white max-1300:px-14 max-900:px-8 bg-footer px-28 pt-9'>
         {/* Footer Links section */}
-        <div className='flex justify-between'>
+        <div className='flex justify-between gap-3 max-900:flex-wrap'>
 
             {/* Logo section*/}
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col gap-5 max-900:basis-1/3'>
                 <img src={logo} alt="Yoff logo" className='w-24'/>
                 <div className=' font-poppins'>© 2024 . All rights reserved.</div>
             </div>
 
-            {/* Quick links */}
-            <div className='flex flex-col gap-5'>
-                <h3 className='text-xl font-semibold font-gilroy'>Quick Links</h3>
+            {/* Quick links */} 
+            <div className='flex flex-col gap-5 max-900:basis-1/3'>
+                <h3 className='text-xl font-semibold max-900:font-bold font-gilroy'>Quick Links</h3>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">Home</a>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">How It Works</a>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">Languages</a>
@@ -32,8 +32,8 @@ const Footer: React.FC = () => {
             </div>
 
             {/* Contact Info */}
-            <div className='flex flex-col gap-5'>
-                <h3 className='text-xl font-semibold font-gilroy'>Contact Information</h3>
+            <div className='flex flex-col gap-5 max-900:basis-1/3 max-900:mt-6'>
+                <h3 className='text-xl font-semibold font-gilroy max-900:font-extrabold'>Contact Information</h3>
                 <div className='flex gap-3 ml-1'>
                     <img className='w-4' src={call} alt="phone icon" />
                     845-123-4567
@@ -49,8 +49,8 @@ const Footer: React.FC = () => {
             </div>
 
             {/* Help */}
-            <div className='flex flex-col gap-5'>
-            <h3 className='text-xl font-semibold font-gilroy'>Help</h3>
+            <div className='flex flex-col gap-5 max-900:basis-1/3 max-900:mt-6'>
+            <h3 className='text-xl font-semibold font-gilroy max-900:font-extrabold'>Help</h3>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">Help Center</a>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">Trust and Safety</a>
                 <a className='font-poppins hover:underline hover:cursor-pointer' href="/">Privacy Settings</a>
@@ -58,21 +58,21 @@ const Footer: React.FC = () => {
         </div>
 
         {/* Footer Bottom section */}
-        <div className='absolute z-50 flex justify-end gap-5 bottom-3 right-28 max-1300:right-14'>
+        <div className='absolute z-50 flex justify-end gap-5 bottom-3 right-28 max-1100:right-4 max-1300:right-14 max-800:right-5'>
             <div>
-                <img className='w-12 hover:cursor-pointer' src={instagram} alt="Instagram icon" />
+                <img className='w-12 max-900:w-10 hover:cursor-pointer max-800:w-11' src={instagram} alt="Instagram icon" />
             </div>
             <div>
-                <img className='w-12 hover:cursor-pointer' src={twitter} alt="Twitter icon" />
+                <img className='w-12 max-900:w-10 hover:cursor-pointer max-800:w-11' src={twitter} alt="Twitter icon" />
             </div>
             <div>
-                <img className='w-12 hover:cursor-pointer' src={linkedIn} alt="Linkedin icon" />
+                <img className='w-12 max-900:w-10 hover:cursor-pointer max-800:w-11' src={linkedIn} alt="Linkedin icon" />
             </div>
         </div>
 
         {/* Footer bottom image */}
-        <div className='absolute bottom-0 right-0 w-full'>
-            <img className='w-full h-20' src={footerBottomBg} alt="Footer image" />
+        <div className='absolute bottom-0 right-0 w-full max-800:right-[83%]'>
+            <img className='w-full h-20 max-800:w-[180vw] max-w-none' src={footerBottomBg} alt="Footer image" />
         </div>
     </footer>
   );
