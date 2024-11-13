a905ccb7297003889763ee87a1e3112b287729de
protoss70 <tlegendgaming@gmail.com>
2024-11-13 16:55:25 +0100

    New teacher setup

--- src/utility/teacherImages.ts
+++ src/utility/teacherImages.ts
@@ -5,6 +5,7 @@ import JunWoo from "../assets/teachers/JunWoo-Kang.webp";
 import Marc from "../assets/teachers/Marc-Nassr.webp";
 import Masha from "../assets/teachers/Maria-Kolosova.webp";
 import Mostafa from "../assets/teachers/Mostafa-Agha.webp";
+import Nam from "../assets/teachers/Hyunuk-Nam.webp";
 
 
 export default {
@@ -12,7 +13,8 @@ export default {
     "Dilum-Ananda": Dilum,
     "JunWoo-Kang":JunWoo,
     "Marc-Nassr":Marc,
-    "Masha-Kolosova":Masha,
+    "Maria-Kolosova":Masha,
     "Mostafa-Agha":Mostafa,
-    "Maria-Seledtsova": maria
+    "Maria-Seledtsova": maria,
+    "Hyunuk-Nam": Nam
 }
